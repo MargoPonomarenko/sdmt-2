6fbcd47fe4d6f1651e31b1a536b568a7cfc58912
Marharyta Ponomarenko <margo.marharet@gmail.com>
2023-03-19 20:21:16 +0200

    feature:edited LinkedList class file

---
+++ Lab2test/Lab2test.cpp
@@ -0,0 +1,53 @@
+// Lab2Studio.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+#include "LinkedList.h"
+
+using namespace std;
+
+int main()
+{
+    LinkedList firstList;
+    firstList.append('2');
+    firstList.append('5');
+    firstList.append('a');
+    firstList.append('f');
+    firstList.append('e');
+    firstList.append('a');
+
+    cout << "First list length: " << firstList.length() << endl;
+    cout << "First list elements: ", firstList.print();
+
+    LinkedList secondList;
+    cout << "Second list length: " << secondList.length() << endl;
+
+    firstList.insert('e', 3);
+    cout << "\nFirst list after insert(): ", firstList.print();
+
+    cout << "Deleted character: " << firstList.deleteChar(0);
+    cout << "\nFirst list after delete(): ", firstList.print();
+    firstList.deleteAll('e');
+    cout << "First list after deleteAll(): ", firstList.print();
+
+    cout << "\nCharacter at given position: " << firstList.get(1);
+
+    LinkedList *clonedList = firstList.clone();
+    cout << "\nCloned list: ", clonedList->print();
+
+    firstList.reverse();
+    cout << "First list after reverse(): ", firstList.print();
+
+    cout << "\nPosition of first search character is: " << firstList.findFirst('a');
+    cout << "\nPosition of last search character is: " << firstList.findLast('a');
+
+    firstList.clear();
+    cout << "\nFirst list after clear(): ", firstList.print();
+
+    secondList.append('5');
+    secondList.append('0');
+    secondList.append('m');
+    secondList.append('a');
+
+    firstList.extend(secondList);
+    cout << "\nFirst list after extend(): ", firstList.print();
+}
---
+++ Lab2test/LinkedList.h
@@ -0,0 +1,166 @@
+#pragma once
+#include <stdexcept>
+
+#include <iostream>
+using namespace std;
+
+class LinkedList
+{
+    char* arr;
+    int capacity;
+    int current;
+
+public:
+    LinkedList()
+    {
+        arr = new char[1];
+        capacity = 1;
+        current = 0;
+    }
+
+    ~LinkedList()
+    {
+        delete[] arr;
+    }
+
+    int length() { return current; }
+
+    void append(char data)
+    {
+
+        if (current == capacity)
+        {
+            char* temp = new char[2 * capacity];
+
+            for (int i = 0; i < capacity; i++)
+            {
+                temp[i] = arr[i];
+            }
+
+            delete[] arr;
+            capacity *= 2;
+            arr = temp;
+        }
+
+        arr[current] = data;
+        current++;
+    }
+
+    char deleteChar(int index)
+    {
+        if (index >= current || index < 0)
+        {
+            throw std::out_of_range{ "Index out of range" };
+        }
+        char removedElement = arr[index];
+        for (int i = index + 1; i < current; ++i)
+        {
+            arr[i - 1] = arr[i];
+        }
+        --current;
+        return removedElement;
+    }
+
+    void deleteAll(char data)
+    {
+        int write_index = 0;
+        for (int i = 0; i < current; ++i)
+        {
+            if (arr[i] != data)
+            {
+                arr[write_index++] = arr[i];
+            }
+        }
+        current = write_index;
+    }
+
+    void insert(char data, int index)
+    {
+        if (index >= current || index < 0)
+        {
+            throw std::out_of_range{ "Index out of range" };
+        }
+        if (index == capacity)
+            append(data);
+        else
+            arr[index] = data;
+    }
+
+    char get(int index)
+    {
+        if (index >= current || index < 0)
+        {
+            throw std::out_of_range{ "Index out of range" };
+        }
+        else
+        {
+            return arr[index];
+        }
+    }
+
+    LinkedList* clone() {
+        LinkedList* copyList = new LinkedList();
+        for (int i = 0; i < current; i++) {
+            copyList->append(arr[i]);
+        }
+        return copyList;
+    }
+
+
+    void reverse()
+    {
+        for (int i = 0; i < current / 2; ++i)
+        {
+            char temp = arr[i];
+            arr[i] = arr[current - i - 1];
+            arr[current - i - 1] = temp;
+        }
+    }
+
+    int findFirst(char item) const
+    {
+        for (int i = 0; i < current; ++i)
+        {
+            if (arr[i] == item)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    int findLast(char item) const
+    {
+        for (int i = current - 1; i >= 0; --i)
+        {
+            if (arr[i] == item)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    void clear()
+    {
+        current = 0;
+    }
+
+    void extend(const LinkedList& other)
+    {
+        for (int i = 0; i < other.current; ++i)
+        {
+            append(other.arr[i]);
+        }
+    }
+    
+    void print()
+    {
+        for (int i = 0; i < current; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
